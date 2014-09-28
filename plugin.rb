7eaa86da4cf16c52cd662364534d64dc1b881364
Jason Zhu <fmy@mail.bistu.edu.cn>
2014-09-29 07:33:18 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -17,46 +17,46 @@ class BistuAuthenticator < ::Auth::OAuth2Authenticator
   end
 end
 
-require 'omniauth-oauth2'
-class OmniAuth::Strategies::BistuOauth < OmniAuth::Strategies::OAuth2
+# require 'omniauth-oauth2'
+# class OmniAuth::Strategies::BistuOauth < OmniAuth::Strategies::OAuth2
 
-  # NOTE VM has to be able to resolve
+#   # NOTE VM has to be able to resolve
   
 
-  # Give your strategy a name.
-  option :name, "bistu"
+#   # Give your strategy a name.
+#   option :name, "bistu"
 
-  # This is where you pass the options you would pass when
-  # initializing your consumer from the OAuth gem.
-  option :client_options, {
-    :site => 'http://222.249.250.234/bistuapi/',
-    :authorize_url => 'http://222.249.250.234/o/authorize/',
-    :token_url => 'http://222.249.250.234/o/token/'
-  }
+#   # This is where you pass the options you would pass when
+#   # initializing your consumer from the OAuth gem.
+#   option :client_options, {
+#     :site => 'http://222.249.250.234/bistuapi/',
+#     :authorize_url => 'http://222.249.250.234/o/authorize/',
+#     :token_url => 'http://222.249.250.234/o/token/'
+#   }
 
-  # These are called after authentication has succeeded. If
-  # possible, you should try to set the UID without making
-  # additional calls (if the user id is returned with the token
-  # or as a URI parameter). This may not be possible with all
-  # providers.
-  uid{ raw_info['id'] }
+#   # These are called after authentication has succeeded. If
+#   # possible, you should try to set the UID without making
+#   # additional calls (if the user id is returned with the token
+#   # or as a URI parameter). This may not be possible with all
+#   # providers.
+#   uid{ raw_info['id'] }
 
-  info do
-    {
-      :name => raw_info['username'],
-      :user => raw_info['userid'],
+#   info do
+#     {
+#       :name => raw_info['username'],
+#       :user => raw_info['userid'],
 
-     }
-  end
+#     }
+#   end
 
-  extra do
-    {'raw_info' => raw_info}
-  end
+#   extra do
+#     {'raw_info' => raw_info}
+#   end
 
-  def raw_info
-    @raw_info ||= access_token.get('/bistuapi/me/').parsed
-  end
-end
+#   def raw_info
+#     @raw_info ||= access_token.get('/bistuapi/me/').parsed
+#   end
+# end
 
 
 auth_provider :title => 'with Bistu',
